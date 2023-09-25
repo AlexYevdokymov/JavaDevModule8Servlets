3177670b64d2720ba3d6a9feb6d1278d7457178b
olye <olexiy.yevdokimov.2000@gmail.com>
2023-09-25 19:45:30 +0300

    Task3 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,6 @@
 package org.example;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class Main {
     public static void main(String[] args) {
-        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern(
-                "Дата: yyy-MM-dd, Час: HH:mm:ss"
-        ));
-        ZoneId zone = ZonedDateTime.now().getOffset();
-        String zone2 = String.valueOf(ZonedDateTime.now(ZoneId.of("+1")));
-        System.out.println(time);
-        System.out.println(zone);
-        System.out.println(zone2);
     }
 }
